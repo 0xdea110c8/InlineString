fabe44bc062613bf56be3b7cc1cd8b6bb771e20d
Ivan A. <alexvanmoor@icloud.com>
2026-07-26 06:18:35 +0400

    Add size, stride

--- Sources/InlineStringBenchmarks/main.swift
+++ Sources/InlineStringBenchmarks/main.swift
@@ -1,1 +1,7 @@
+import Benchmark
+import InlineString
 
+print("InlineString16 size:", MemoryLayout<InlineString16>.size)
+print("InlineString16 stride:", MemoryLayout<InlineString16>.stride)
+
+Benchmark.main()
