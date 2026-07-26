904b785be89df6d254eb97d6b9f9c5b5f1833aef
Ivan A. <alexvanmoor@icloud.com>
2026-07-26 09:13:46 +0400

    Add empty init benchmark

--- Sources/InlineStringBenchmarks/InitBenchmarks.swift
+++ Sources/InlineStringBenchmarks/InitBenchmarks.swift
@@ -3,7 +3,21 @@ import InlineString
 
 nonisolated(unsafe) var sourceString = "Berlin"
 
-let initBenchmarks = BenchmarkSuite(name: "Init") { suite in
+let initBenchmarks = BenchmarkSuite(name: "init") { suite in
+    suite.benchmark("inline-string-empty") {
+        for _ in 0..<1_000_000 {
+            let value = InlineString16()
+            consumeInline(value)
+        }
+    }
+
+    suite.benchmark("string-empty") {
+        for _ in 0..<1_000_000 {
+            let value = ""
+            consumeString(value)
+        }
+    }
+    
     suite.benchmark("inline-string-truncating") {
         for _ in 0..<1_000_000 {
             let value = InlineString16(truncating: sourceString)
