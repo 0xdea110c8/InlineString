68724fe9a5d685b94275544f86a088e1bc501828
Ivan A. <alexvanmoor@icloud.com>
2026-07-28 01:02:31 +0400

    Add init benchmarks

--- Sources/InlineStringBenchmarks/InitBenchmarks.swift
+++ Sources/InlineStringBenchmarks/InitBenchmarks.swift
@@ -3,6 +3,8 @@ import InlineString
 
 fileprivate nonisolated(unsafe) var emptyString = ""
 fileprivate nonisolated(unsafe) var sourceString = "Berlin"
+fileprivate nonisolated(unsafe) var sourceString15 = "123456789012345"
+fileprivate nonisolated(unsafe) var sourceString16 = "1234567890123456"
 
 let initEmptyComparison = BenchmarkSuite(name: "init-empty") { suite in
     suite.benchmark("string") {
@@ -73,6 +75,80 @@ let initComparison = BenchmarkSuite(name: "init") { suite in
     suite.benchmark("inline-string-string-literal") {
         for _ in 0..<1_000_000 {
             let value: InlineString16 = "Berlin"
+            consumeInline(value)
+        }
+    }
+}
+
+let init15Comparison = BenchmarkSuite(name: "init-15-bytes") { suite in
+    suite.benchmark("string") {
+        for _ in 0..<1_000_000 {
+            let value = sourceString15
+            consumeString(value)
+        }
+    }
+    
+    suite.benchmark("string-copy") {
+        for _ in 0..<1_000_000 {
+            let value = String(sourceString15)
+            consumeString(value)
+        }
+    }
+    
+    suite.benchmark("inline-string") {
+        for _ in 0..<1_000_000 {
+            let value = InlineString16(sourceString15)
+            consumeInline(value)
+        }
+    }
+    
+    suite.benchmark("inline-string-validating") {
+        for _ in 0..<1_000_000 {
+            let value = InlineString16(validating: sourceString15)!
+            consumeInline(value)
+        }
+    }
+    
+    suite.benchmark("inline-string-string-literal") {
+        for _ in 0..<1_000_000 {
+            let value: InlineString16 = "123456789012345"
+            consumeInline(value)
+        }
+    }
+}
+
+let init16Comparison = BenchmarkSuite(name: "init-16-bytes") { suite in
+    suite.benchmark("string") {
+        for _ in 0..<1_000_000 {
+            let value = sourceString16
+            consumeString(value)
+        }
+    }
+    
+    suite.benchmark("string-copy") {
+        for _ in 0..<1_000_000 {
+            let value = String(sourceString16)
+            consumeString(value)
+        }
+    }
+    
+    suite.benchmark("inline-string") {
+        for _ in 0..<1_000_000 {
+            let value = InlineString16(sourceString16)
+            consumeInline(value)
+        }
+    }
+    
+    suite.benchmark("inline-string-validating") {
+        for _ in 0..<1_000_000 {
+            let value = InlineString16(validating: sourceString16)!
+            consumeInline(value)
+        }
+    }
+    
+    suite.benchmark("inline-string-string-literal") {
+        for _ in 0..<1_000_000 {
+            let value: InlineString16 = "1234567890123456"
             consumeInline(value)
         }
     }
