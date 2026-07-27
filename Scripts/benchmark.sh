d388f65eea8202960291e1c9a6df3a76427b9539
Ivan A. <alexvanmoor@icloud.com>
2026-07-27 04:27:21 +0400

    Update benchmark script

--- Scripts/benchmark.sh
+++ Scripts/benchmark.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-swift run -c release InlineStringBenchmarks
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+RESULTS_DIR="$SCRIPT_DIR/benchmarks"
+
+mkdir -p "$RESULTS_DIR"
+swift run -c release InlineStringBenchmarks | sed -n '/^name[[:space:]]/,/^$/p' > "$RESULTS_DIR/benchmarks-$(git rev-parse --short HEAD).txt"
