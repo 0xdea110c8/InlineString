5ddb8ebd3607121c36f80219674c7001fd83decb
Ivan A. <alexvanmoor@icloud.com>
2026-08-12 19:12:33 +0400

    refactor access benchmarks

--- Benchmarks/InlineStringBenchmark/Benchmarks/Access/Count/AccessCount.swift
+++ Benchmarks/InlineStringBenchmark/Benchmarks/Access/Count/AccessCount.swift
@@ -36,38 +36,3 @@ let accessCount: @Sendable () -> Void = {
         }
     }
 }
-
-let accessString: @Sendable () -> Void = {
-    let string: String = "1234567890"
-    let inlineString: InlineString16 = "1234567890"
-
-    Benchmark(
-        "access/string/string",
-        configuration: .init(
-            metrics: [.wallClock, .instructions],
-            timeUnits: .nanoseconds
-        )
-    ) { benchmark in
-        for _ in benchmark.scaledIterations {
-            for _ in 0..<1_000_000 {
-                let value = string
-                consumeString(value)
-            }
-        }
-    }
-
-    Benchmark(
-        "access/string/inlinestring16",
-        configuration: .init(
-            metrics: [.wallClock, .instructions],
-            timeUnits: .nanoseconds
-        )
-    ) { benchmark in
-        for _ in benchmark.scaledIterations {
-            for _ in 0..<1_000_000 {
-                let value = inlineString.string
-                consumeString(value)
-            }
-        }
-    }
-}
