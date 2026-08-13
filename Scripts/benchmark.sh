5daf4c882b83ab927c0a764930586cdd95122216
Ivan A. <alexvanmoor@icloud.com>
2026-08-13 16:23:01 +0400

    remove scripts

--- Scripts/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-set -e
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-RESULTS_DIR="$SCRIPT_DIR/benchmarks"
-
-mkdir -p "$RESULTS_DIR"
-swift run -c release InlineStringBenchmarks | sed -n '/^name[[:space:]]/,/^$/p' > "$RESULTS_DIR/benchmarks-$(git rev-parse --short HEAD).txt"
--- Scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-set -e
-
-swift test
